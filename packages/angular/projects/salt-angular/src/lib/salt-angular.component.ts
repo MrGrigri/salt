8f920cb26585b3f8cd75a8cba8856857c8e3a589
Michael Richins <michael.richins@progleasing.com>
2024-04-03 09:56:27 -0600

    Add: @richkode/angular

---
+++ packages/angular/projects/salt-angular/src/lib/salt-angular.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-salt-angular',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      salt-angular works!
+    </p>
+  `,
+  styles: ``
+})
+export class SaltAngularComponent {
+
+}
---
+++ packages/angular/projects/salt-angular/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of salt-angular
+ */
+
+export * from './lib/salt-angular.service';
+export * from './lib/salt-angular.component';
