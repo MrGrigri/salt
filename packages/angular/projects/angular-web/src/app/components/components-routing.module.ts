d7ce9f3749c64d6efc312f06d31fc80f7b040617
Michael Richins <michael.richins@progleasing.com>
2024-04-04 14:42:06 -0600

    Add: component routes

--- packages/angular/projects/angular-web/src/app/components/components-routing.module.ts
+++ packages/angular/projects/angular-web/src/app/components/components-routing.module.ts
@@ -2,7 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComponentsComponent } from './components.component';
 
-const routes: Routes = [{ path: '', component: ComponentsComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ComponentsComponent,
+    children: [
+      { path: '', redirectTo: 'my-component', pathMatch: 'full' },
+      {
+        path: 'my-component',
+        loadChildren: () =>
+          import('./my-component/my-component.module').then(
+            (m) => m.MyComponentModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ packages/angular/projects/angular-web/src/app/components/my-component/my-component-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MyComponentComponent } from './my-component.component';
+
+const routes: Routes = [{ path: '', component: MyComponentComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MyComponentRoutingModule { }
