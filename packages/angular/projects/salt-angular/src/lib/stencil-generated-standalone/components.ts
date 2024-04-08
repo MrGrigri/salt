b9d820e53e41ee751ac3ad8f0af5ed1181b3f743
Michael Richins <michael.richins@progleasing.com>
2024-04-08 12:27:51 -0600

    Add: Angular Standalone output

---
+++ packages/angular/projects/salt-angular/src/lib/stencil-generated-standalone/components.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* auto-generated angular directive proxies */
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone } from '@angular/core';
+
+import { ProxyCmp } from './angular-component-lib/utils';
+
+/* Import rewritten by fixAngularStandaloneComponents */
+import type { Components } from '@richkode/salt';
+
+import { defineCustomElement as defineMyComponent } from '@richkode/salt/components/my-component.js';
+@ProxyCmp({
+  defineCustomElementFn: defineMyComponent,
+  inputs: ['first', 'last', 'middle']
+})
+@Component({
+  selector: 'my-component',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  // eslint-disable-next-line @angular-eslint/no-inputs-metadata-property
+  inputs: ['first', 'last', 'middle'],
+  standalone: true
+})
+export class MyComponent {
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+  }
+}
+
+
+export declare interface MyComponent extends Components.MyComponent {}
+
+
--- packages/angular/projects/salt-angular/src/public-api.ts
+++ packages/angular/projects/salt-angular/src/public-api.ts
@@ -2,6 +2,10 @@
  * Public API Surface of salt-angular
  */
 
+// Module components
 export * from './lib/salt.module';
 export { DIRECTIVES } from './lib/stencil-generated';
 export * from './lib/stencil-generated/components';
+
+// Standalone Components
+export { DIRECTIVES as COMPONENTS } from './lib/stencil-generated-standalone';
